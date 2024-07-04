5b9ecbf4d02a0ba82ccb0a2d4ac186c21b70a2b3
Jonas <xgv866@alumni.ku.dk>
2024-07-04 20:30:57 +0200

    change js file

--- src/javascript/sketch.js
+++ src/javascript/sketch.js
@@ -1,29 +1,69 @@
-let scene, camera, renderer, mesh;
+var container,
+  renderer,
+  scene,
+  camera,
+  mesh,
+  material,
+  start = Date.now(),
+  fov = 30;
 
-function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-    
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-    
-    camera.position.z = 5;
+window.addEventListener('load', function() {
+  // grab the container from the DOM
+  container = document.getElementById("container");
+
+  // create a scene
+  scene = new THREE.Scene();
+
+  // create a camera the size of the browser window
+  // and place it 100 units away, looking towards the center of the scene
+  camera = new THREE.PerspectiveCamera(
+    fov,
+    window.innerWidth / window.innerHeight,
+    1,
+    10000
+  );
+  camera.position.z = 100;
+
+  // create a shader material
+  material = new THREE.ShaderMaterial({
+    uniforms: {
+      time: { // float initialized to 0
+        type: "f",
+        value: 0.0
+      }
+    },
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent
+  });
+
+  // create a sphere and assign the material
+  mesh = new THREE.Mesh(
+    new THREE.IcosahedronGeometry(20, 4),
+    material
+  );
+  scene.add(mesh);
+
+  // create the renderer and attach it to the DOM
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  container.appendChild(renderer.domElement);
+
+  // Add window resize listener
+  window.addEventListener('resize', onWindowResize, false);
+
+  render();
+});
+
+function render() {
+  material.uniforms['time'].value = .00025 * (Date.now() - start);
+  // let there be light
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
 }
 
-function animate() {
-    requestAnimationFrame(animate);
-    
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
-    
-    renderer.render(scene, camera);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
-
-init();
-animate();
